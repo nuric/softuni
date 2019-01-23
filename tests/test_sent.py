f35379b22ef4d83c5dbb553136fcfb713b663597
nuric <nuric@users.noreply.github.com>
2019-01-23 00:15:48 +0000

    Updated sentence tests to reflect variables property change.

--- tests/test_sent.py
+++ tests/test_sent.py
@@ -9,19 +9,19 @@ class TestSent(unittest.TestCase):
     """Check parsing on no variable sentence."""
     s = Sent.from_text("Alice went to the hallway.")
     self.assertEqual(len(s), 5)
-    self.assertFalse(s.variables)
+    self.assertFalse(s.variables[1])
 
   def test_creation_from_text_var(self):
     """Check parsing with variables."""
     s = Sent.from_text("X:Alice went to the Y:hallway.")
     self.assertEqual(len(s), 5)
-    self.assertEqual(len(s.variables), 2)
+    self.assertEqual(len(s.variables[1]), 2)
 
   def test_creation_from_text_merged(self):
     """Check parsing with merging variables."""
     s = Sent.from_text("How is the weather in X:New X:York?")
     self.assertEqual(len(s), 6)
-    self.assertEqual(len(s.variables), 1)
+    self.assertEqual(len(s.variables[1]), 1)
 
   def test_empty_similarity(self):
     """Similarity is zero if sentence is empty."""
@@ -45,13 +45,14 @@ class TestSent(unittest.TestCase):
     """Check if no variable sentence fails to unify."""
     s = Sent.from_text("")
     o = Sent.from_text("Alice went to the kitchen.")
-    self.assertEqual(s.unify(o), 0.0)
+    self.assertEqual(s.unify(o), 1.0)
 
   def test_empty_unification(self):
     """Check if unification fails with empty string."""
     s = Sent.from_text("X:Alice went to the Y:kitchen.")
     o = Sent.from_text("")
-    self.assertEqual(s.unify(o), 0.0)
+    self.assertEqual(str(s[0]), "None")
+    self.assertEqual(str(s[-1]), "None")
 
   def test_single_var_unify(self):
     """Check if unification matches correct token."""
